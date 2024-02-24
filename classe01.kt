7fa872be26306bef16567783612c87e3c04227e2
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2024-02-23 21:55:02 -0300

    .

---
+++ classe01.kt
@@ -0,0 +1,10 @@
+class musica {
+    val título = "carry on"    
+    val artista = "angra" 
+    val anodepublicação = "1999"  
+    val foitocada = "1000" 
+    
+    fun exebir() {
+        
+    }
+}
