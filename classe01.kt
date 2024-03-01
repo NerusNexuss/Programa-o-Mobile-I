1c726df030a493926aad614e00e4a0796a0b1586
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2024-03-01 19:42:49 -0300

    .
    .

--- classe01.kt
+++ classe01.kt
@@ -1,10 +1,12 @@
-class musica {
-    val título = "carry on"    
-    val artista = "angra" 
-    val anodepublicação = "1999"  
-    val foitocada = "1000" 
-    
-    fun exebir() {
-        
+class Musica (val título: String, val artista: String, val ano:Int, var vezesTocadas:Int) {
+    val isPopular: Boolean
+        get() = vezesTocadas >= 1000
+    fun imprimir() {
+        println("$título, realizado por $artista, foi lançado em $ano.")
     }
+}
+fun main(){
+    val musica = Musica(título = "enemy", artista = "Imagine Dragons", ano = 2022, vezesTocadas = 100)
+    musica.imprimir()
+    println(musica.isPopular)
 }
